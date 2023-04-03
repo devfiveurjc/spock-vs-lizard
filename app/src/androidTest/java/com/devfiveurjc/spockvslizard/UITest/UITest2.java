91dcfeec7199962b8047e71eabbc8124dbafc653
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2023-04-03 22:47:11 +0200

    UI test update
    Detected an error while running tests. Not fixed

--- app/src/androidTest/java/com/devfiveurjc/spockvslizard/UITest/UITest2.java
+++ app/src/androidTest/java/com/devfiveurjc/spockvslizard/UITest/UITest2.java
@@ -2,10 +2,12 @@ package com.devfiveurjc.spockvslizard.UITest;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
@@ -32,5 +34,8 @@ public class UITest2 {
         String value="1 vs Maquina";
         onView(withId(R.id.option1)).check(matches(isDisplayed())); //al cargar la activity, comprueba que el elemento exista
         onView(withId(R.id.option1)).check(matches(withText(value))); //comprueba que si existe, tiene este texto
+
+        onView(withId(R.id.ajustes)).check(matches(isDisplayed())); //al cargar la activity, comprueba que el elemento exista
+        onView(withId(R.id.ajustes)).check((ViewAssertion)(isClickable()));
     }
 }
--- app/src/androidTest/java/com/devfiveurjc/spockvslizard/UITest/UITest3.java
+++ app/src/androidTest/java/com/devfiveurjc/spockvslizard/UITest/UITest3.java
@@ -1,8 +1,10 @@
 package com.devfiveurjc.spockvslizard.UITest;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -33,8 +35,14 @@ public class UITest3 {
     @Test
     public void correctText(){
         String value = "Jugador";
+        onView(withId(R.id.nombreIngresado)).check(matches(isDisplayed()));
         onView(withId(R.id.nombreIngresado)).perform(typeText(value), ViewActions.closeSoftKeyboard()); //escribe en un campo de texto el nombre jugador
         onView(withId(R.id.nombreIngresado)).check(matches(withText(value))); //se comprueba que el elemento que almacena el texto, tiene el contenido introducido
+
+        onView(withId(R.id.option1)).perform(click()); //se hace clic para cambiar a la actividad principal
+        onView(withId(R.id.username)).check(matches(isDisplayed())); //se comprueba que el campo con el texto se muestra
+        onView(withId(R.id.username)).check(matches(withText(value))); //se comprueba que el campo tenga el valor del nombre
+
     }
 }
 
